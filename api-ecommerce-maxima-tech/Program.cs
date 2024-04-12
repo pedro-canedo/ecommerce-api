e80ab2ba1e019c8b1ca059fb825b608ca95ee047
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2024-04-12 13:07:55 -0300

    chore: adicionando configurações de ambiênte

--- api-ecommerce-maxima-tech/Program.cs
+++ api-ecommerce-maxima-tech/Program.cs
@@ -24,8 +24,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ecommerce Maxima Tech API-V1"));
 }
+
 
 app.UseHttpsRedirection();
 app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
